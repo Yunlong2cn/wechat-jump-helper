b124c179638b19d934e7cc4aca83833d35d2e3f3
Yunlong2cn <384345134@qq.com>
2018-01-20 01:48:16 +0800

    优化色块中心点坐标获取

--- src/main/java/com/github/yunlong2cn/App.java
+++ src/main/java/com/github/yunlong2cn/App.java
@@ -27,26 +27,65 @@ public class App {
             if(exitCode != 0) {
                 System.out.println("[-] jump.png generate failed");
                 isJump = false;
-                return;
+                continue;
             }
             cmd = "adb pull /sdcard/jump.png " + path;
             process = runtime.exec(cmd);
             exitCode = process.waitFor();
             if(exitCode != 0) {
                 System.out.println("[-] jump.png pulled failed");
-            }
-
-            isJump = false;
-
-            Color[][] colors = ImageHelper.toColors(path + "/" + "jump.png");
-
-            Color startColor = colors[0][0];
-            if(startColor.equals(new Color(51,46,44))) {
                 isJump = false;
                 continue;
             }
 
+            Color[][] colors = ImageHelper.toColors(path + "/" + "jump.png");
+
             Color backColor = colors[0][288];
+
+            if(backColor.equals(new Color(51,49,36))) {
+                isJump = false;
+                System.out.println("[+] Game over");
+                continue;
+            }
+
+
+            // 计算棋子坐标
+            // 自下往上，自右往左（因为棋子是分开的，自下往上能快速定位到棋子底部，自右往左，是为了避免棋子阴影造成的干扰）
+            // 先计算棋子坐标，再计算色块坐标（由于棋子有可能高于色块位置，所以需要用棋子的 x 坐标来判断色块坐标的正确性）
+            Position pieceCenter = new Position();
+            Color pieceColor = new Color(57,57,99); // 认为棋子的RGB颜色值应为 pieceColor
+            for (int y = colors[0].length - 1; y >= 0; y--) {
+                if(pieceCenter.getX() > 0) {
+                    break;
+                } else {
+                    for (int x = colors.length - 1; x >= 0; x--) {
+                        double sim = ImageHelper.sim(pieceColor, colors[x][y]);
+                        int distance = Math.abs(pieceCenter.getX() - x);// 从 x 坐标看距离棋子的长度
+                        if(sim < 1 && distance > 50) {
+                            pieceCenter.setY(y - 9);
+                            pieceCenter.setX(x - 21);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            System.out.println("[+] pieceCenter = " + pieceCenter);
+
+            if(pieceCenter.getX() == 0 || pieceCenter.getY() == 0) {
+                System.out.println("[-] Have no piece on dashboard");
+                cmd = "adb shell input tap 555 1575";
+                if(runtime.exec(cmd).waitFor() == 0) {
+                    System.out.println("[+] Restart Game success");
+                } else {
+                    System.out.println("[-] Restart Game failed");
+                }
+                continue;
+            }
+
+
+
+
 
             Position blockStart = new Position(); // 色块起始位置
             Position blockLeft = new Position(); // 色块左侧位置
@@ -66,16 +105,24 @@ public class App {
 
                                 blockLeft.setX(x);
                                 blockLeft.setY(y);
+
+                                break;// 一旦在当前行发现了想要的位置，则记录后跳出并进行下一行处理
                             } else {
                                 if(color.equals(blockStart.getColor())) {// 如果当前 color 与 start 位置的 color 相同，说明是 left 的起始位置
-                                    if(blockLeft.getX() >= x && blockLeft.getY() < y) {// 上一次保存的 left 位置如果小于这次的 x 位置，则说明 left 并未取到最左侧
+                                    int yDistance = y - blockLeft.getY();// 当前颜色值的 y 坐标与上一次定义为左侧位置的 y 坐标的距离
+                                    // 认为：当 yDistance < 阈值时，则是同一个色块
+                                    // 认为：上一次保存位置 x 坐标应大于或等于当前位置 x 坐标
+                                    // 认为：上一次保存位置 y 坐标应小于当前位置 y 坐标
+                                    if(blockLeft.getX() >= x && blockLeft.getY() < y && yDistance < 200) {// 上一次保存的 left 位置如果小于这次的 x 位置，则说明 left 并未取到最左侧
                                         blockLeft.setX(x);
                                         blockLeft.setY(y);
+
+                                        break;// 一旦在当前行发现了想要的位置，则记录后跳出并进行下一行处理
                                     }
                                 }
                             }
 
-                            break;// 一旦在当前行发现了想要的位置，则记录后跳出并进行下一行处理
+
                         }
 
                     }
@@ -88,28 +135,6 @@ public class App {
 
 
             System.out.println("[+] blockCenter = " + blockCenter);
-
-
-            // 计算棋子坐标
-            // 自下往上，自右往左（因为棋子是分开的，自下往上能快速定位到棋子底部，自右往左，是为了避免棋子阴影造成的干扰）
-            Position pieceCenter = new Position();
-            Color pieceColor = new Color(57,57,99); // 认为棋子的RGB颜色值应为 pieceColor
-            for (int y = colors[0].length - 1; y >= 0; y--) {
-                if(pieceCenter.getX() > 0) {
-                    break;
-                } else {
-                    for (int x = colors.length - 1; x >= 0; x--) {
-                        double sim = ImageHelper.sim(pieceColor, colors[x][y]);
-                        if(sim < 1) {
-                            pieceCenter.setY(y - 9);
-                            pieceCenter.setX(x - 21);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            System.out.println("[+] pieceCenter = " + pieceCenter);
 
             int x = Math.abs(pieceCenter.getX() - blockCenter.getX());
             int y = Math.abs(pieceCenter.getY() - blockCenter.getY());
@@ -125,12 +150,11 @@ public class App {
 
             Random random = new Random();
             int second = random.nextInt(5);
+            second = second < 2 ? 2 : second;
             System.out.println("[+] wait for "+ second +" senconds");
             Thread.sleep(second * 1000);
-
-            isJump = true;
         }
 
-        System.out.println("[+] quit");
+        System.out.println("[+] Quit");
     }
 }
